c842e79254417c70a420090895a591cba4e50952
SSSerg05 <ssserg05@gmail.com>
2023-06-05 08:49:15 +0300

    fix

--- src/js/about-Gallery-Genre.js
+++ src/js/about-Gallery-Genre.js
@@ -9,7 +9,7 @@ const refs = {
 
 // екземпляр класа до відображення трендових фільмів на день
 const moviesTrendsDay = new Gallery({
-  name: 'moviesTrendsDay',
+  name: 'MoviesTrendsDay',      // назва гілки у LS
   selector: ".gallery",         // куди виводимо сформований HTML-код 
   url: '/trending/movie/day',   // частина шляху для запиту
   query: 'language=en'          // сам запит, те що стоъть після знаку ?
